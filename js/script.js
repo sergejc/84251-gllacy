d7c4843c7f7bcf042868f5760a4a41178cd407aa
Sergej Charskij <sergej.charskij@gmail.com>
2018-03-28 21:42:20 +0100

    Update the slider

--- js/script.js
+++ js/script.js
@@ -1,1 +1,19 @@
-console.log("Hello Gllacy")
+function changeBackground(el) {
+    var classList = [
+        'site-wrapper-background1',
+        'site-wrapper-background2',
+        'site-wrapper-background3'
+    ];
+    var num = parseInt(
+        el.getAttribute('for').replace(/[^0-9]/g, '')
+    );
+    var wrapper = document.querySelector('.site-wrapper');
+    console.log(el);
+    wrapper.classList = 'site-wrapper ' + classList[num - 1];
+}
+
+document.querySelectorAll('.slider__label').forEach(function (el) {
+    el.addEventListener('click', function (e) {
+        changeBackground(this);
+    });
+});
