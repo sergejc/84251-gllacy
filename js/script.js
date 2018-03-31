f2d7aeb30b1d49138d524c7d22459be1a6a34f82
Sergej Charskij <sergej.charskij@gmail.com>
2018-03-31 21:07:40 +0100

    Updated the delay for menu

--- js/script.js
+++ js/script.js
@@ -1,19 +1,43 @@
 function changeBackground(el) {
     var classList = [
-        'site-wrapper-background1',
-        'site-wrapper-background2',
-        'site-wrapper-background3'
+        'site-background1',
+        'site-background2',
+        'site-background3'
     ];
     var num = parseInt(
         el.getAttribute('for').replace(/[^0-9]/g, '')
     );
-    var wrapper = document.querySelector('.site-wrapper');
-    console.log(el);
-    wrapper.classList = 'site-wrapper ' + classList[num - 1];
+    var wrapper = document.querySelector('body');
+    wrapper.classList = classList[num - 1];
 }
 
 document.querySelectorAll('.slider__label').forEach(function (el) {
     el.addEventListener('click', function (e) {
         changeBackground(this);
+    });
+});
+
+document.querySelectorAll('.site-nav__link').forEach(function(el) {
+    el.addEventListener('mouseenter', function (e) {
+        e.target.closest('.site-nav__item').classList.add('site-nav__item--active');
+    });
+
+    el.addEventListener('mouseleave', function (e) {
+        setTimeout(function () {
+            e.target.closest('.site-nav__item').classList.remove('site-nav__item--active');
+        }, 300);
+    });
+});
+
+
+document.querySelectorAll('.user-nav__search, .user-nav__registration, .shopping-card__quantity').forEach(function(el) {
+    el.addEventListener('mouseenter', function (e) {
+        e.target.closest('.user-nav__item').classList.add('user-nav__item--active');
+    });
+
+    el.addEventListener('mouseleave', function (e) {
+        setTimeout(function () {
+            e.target.closest('.user-nav__item').classList.remove('user-nav__item--active');
+        }, 300);
     });
 });
